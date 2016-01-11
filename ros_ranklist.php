042ee0191948b6556042fcbaa9b2cb6b8e88c8da
sai <saimageshvar@gmail.com>
2016-01-11 18:02:14 +0530

    final ranklist

--- ros_ranklist.php
+++ ros_ranklist.php
@@ -4,11 +4,13 @@
 	$conn=mysqli_connect($host,$mysql_u,$mysql_p,$mysql_db);
 	if($conn)
 	{
-		$query = sprintf("SELECT a1.k_id, a1.level, a1.start_time , count(a2.level) Rank 
+		$set_row_number=sprintf("set @row_number=0");
+		mysqli_query($conn,$set_row_number);
+		$query = sprintf("SELECT (@row_number:=@row_number + 1) AS rank,k_id,name from (select a1.k_id,a1.name, a1.level, a1.start_time , count(a2.level) 
 		FROM ros_users a1, ros_users a2 
 		WHERE a1.level < a2.level OR (a1.level=a2.level AND a1.k_id = a2.k_id) 
 		GROUP BY a1.k_id, a1.level 
-		ORDER BY a1.level DESC, a1.start_time asc;");
+		ORDER BY a1.level DESC, a1.start_time asc) as ranklist;");
 		
 		$ros_ranklist=mysqli_query($conn,$query);
 		$ranklist=array();
@@ -24,4 +26,4 @@
 		mysqli_close($conn);
 		
 		
-	}			
+	}				
--- ros_use_lifeline.php
+++ ros_use_lifeline.php
@@ -5,7 +5,7 @@
 	$con=mysqli_connect($host,$mysql_u,$mysql_p,$mysql_db);
 	if($con)
 	{
-		$query = sprintf("update ros_users set level=level+1 where k_id='%s' and level < 10;",$_GET['k_id']);
+		$query = sprintf("update ros_users set level=level+1 where k_id='%s' and level < 10;",$_POST['k_id']);
 		mysqli_query($con,$query);
 		getQues($con);
 		mysqli_close($con);
